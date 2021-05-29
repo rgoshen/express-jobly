9b285d5de7a7bce8eb44ee0ed71243dfd8ceafbb
rgoshen <rick.goshen@gmail.com>
2021-05-28 20:13:50 -0700

    add filter to get all companies

--- routes/companies.js
+++ routes/companies.js
@@ -14,7 +14,6 @@ const companyUpdateSchema = require("../schemas/companyUpdate.json");
 
 const router = new express.Router();
 
-
 /** POST / { company } =>  { company }
  *
  * company should be { handle, name, description, numEmployees, logoUrl }
@@ -28,7 +27,7 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyNewSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -51,8 +50,15 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  */
 
 router.get("/", async function (req, res, next) {
+  const queryObj = req.query;
+
+  if (queryObj.minEmployees !== undefined)
+    queryObj.minEmployees = +queryObj.minEmployees;
+  if (queryObj.maxEmployees !== undefined)
+    queryObj.maxEmployees = +queryObj.maxEmployees;
+
   try {
-    const companies = await Company.findAll();
+    const companies = await Company.findAll(queryObj);
     return res.json({ companies });
   } catch (err) {
     return next(err);
@@ -91,7 +97,7 @@ router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyUpdateSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -115,6 +121,5 @@ router.delete("/:handle", ensureLoggedIn, async function (req, res, next) {
     return next(err);
   }
 });
-
 
 module.exports = router;
