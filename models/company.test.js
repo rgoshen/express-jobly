0a750fec67061cdcff1ee6c28f4494091258a774
rgoshen <rick.goshen@gmail.com>
2021-05-29 11:46:27 -0700

    add max employees test

--- models/company.test.js
+++ models/company.test.js
@@ -106,6 +106,26 @@ describe("findAll", function () {
       },
     ]);
   });
+
+  test("works: by max employees", async function () {
+    let companies = await Company.findAll({ maxEmployees: 2 });
+    expect(companies).toEqual([
+      {
+        handle: "c1",
+        name: "C1",
+        description: "Desc1",
+        numEmployees: 1,
+        logoUrl: "http://c1.img",
+      },
+      {
+        handle: "c2",
+        name: "C2",
+        description: "Desc2",
+        numEmployees: 2,
+        logoUrl: "http://c2.img",
+      },
+    ]);
+  });
 });
 
 /************************************** get */
