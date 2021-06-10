477db6bdf48d2013a8b45918edd021f1d64d03cd
rgoshen <rick.goshen@gmail.com>
2021-06-10 15:00:17 -0700

    update all companies routes documentation

--- routes/companies.js
+++ routes/companies.js
@@ -97,7 +97,7 @@ router.get("/:handle", async function (req, res, next) {
  *
  * Returns { handle, name, description, numEmployees, logo_url }
  *
- * Authorization required: login
+ * Authorization required: admin
  */
 
 router.patch("/:handle", ensureAdmin, async function (req, res, next) {
@@ -117,7 +117,7 @@ router.patch("/:handle", ensureAdmin, async function (req, res, next) {
 
 /** DELETE /[handle]  =>  { deleted: handle }
  *
- * Authorization: login
+ * Authorization: admin
  */
 
 router.delete("/:handle", ensureAdmin, async function (req, res, next) {
